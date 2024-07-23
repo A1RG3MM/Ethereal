abc3acc1dfd741765bb757b4c99760fe436c6be5
Xordas <120686481+Xordas@users.noreply.github.com>
2024-07-23 13:37:15 +0000

    Add new flash games to the games list

--- public/games.js
+++ public/games.js
@@ -37,6 +37,48 @@ let games = [
         "src": "./game-files/flash-files/ltf3.swf", 
         "image": "./game-files/img/learntofly3.png"
     },
+    {
+        "name": "ducklife",
+        "type": "flash",
+        "src": "./game-files/flash-files/dl.swf",
+        "image": "./game-files/img/ducklife.png"
+    },
+    {
+        "name": "ducklife2",
+        "type": "flash",
+        "src": "./game-files/flash-files/dl2.swf",
+        "image": "./game-files/img/ducklife2.png"
+    },
+    {
+        "name": "ducklife3",
+        "type": "flash",
+        "src": "./game-files/flash-files/dl3.swf",
+        "image": "./game-files/img/ducklife3.png"
+    },
+    {
+        "name": "ducklife4",
+        "type": "flash",
+        "src": "./game-files/flash-files/dl4.swf", 
+        "image": "./game-files/img/ducklife4.png"
+    },
+    {
+        "name": "btd",
+        "type": "flash",
+        "src": "./game-files/flash-files/btd.swf",
+        "image": "./game-files/img/btd.png"
+    },
+    {
+        "name": "btd2",
+        "type": "flash",
+        "src": "./game-files/flash-files/btd2.swf",
+        "image": "./game-files/img/btd2.png"
+    },
+    {
+        "name": "btd3",
+        "type": "flash",
+        "src": "./game-files/flash-files/btd3.swf", 
+        "image": "./game-files/img/btd3.png"
+    },
 
 
 ]
