48591216f0cfc81973c08bce67e31ef19d936673
Xordas <120686481+Xordas@users.noreply.github.com>
2024-07-23 16:59:47 +0000

    Update names in games list to be full names

--- public/games.js
+++ public/games.js
@@ -2,79 +2,79 @@
 let games = [
 
     {
-        "name": "drivemad",
+        "name": "Drive Mad",
         "type": "local",
         "src": "./game-files/drivemad/index.html",
         "image": "./game-files/img/drivemad.png"
     },
     {
-        "name": "polytrack",
+        "name": "Poly Track",
         "type": "local",
         "src": "./game-files/polytack/index.html",
         "image": "./game-files/img/polytrack.png"
     },
     {
-        "name": "funnyshooter2",
+        "name": "Funny Shooter 2",
         "type": "local",
         "src": "./game-files/funnyshooter2/index.html",
         "image": "./game-files/img/funnyshooter2.png"
     },
     {
-        "name": "learntofly",
+        "name": "Learn to Fly",
         "type": "flash",
         "src": "./game-files/flash-files/ltf.swf",
         "image": "./game-files/img/learntofly.png"
     },
     {
-        "name": "learntofly2",
+        "name": "Learn to Fly 2",
         "type": "flash",
         "src": "./game-files/flash-files/ltf2.swf",
         "image": "./game-files/img/learntofly2.png"
     },
     {
-        "name": "learntofly3",
+        "name": "Learn to Fly 3",
         "type": "flash",
         "src": "./game-files/flash-files/ltf3.swf", 
         "image": "./game-files/img/learntofly3.png"
     },
     {
-        "name": "ducklife",
+        "name": "Duck Life",
         "type": "flash",
         "src": "./game-files/flash-files/dl.swf",
         "image": "./game-files/img/ducklife.png"
     },
     {
-        "name": "ducklife2",
+        "name": "Duck Life 2",
         "type": "flash",
         "src": "./game-files/flash-files/dl2.swf",
         "image": "./game-files/img/ducklife2.png"
     },
     {
-        "name": "ducklife3",
+        "name": "Duck Life 3",
         "type": "flash",
         "src": "./game-files/flash-files/dl3.swf",
         "image": "./game-files/img/ducklife3.png"
     },
     {
-        "name": "ducklife4",
+        "name": "Duck Life 4",
         "type": "flash",
         "src": "./game-files/flash-files/dl4.swf", 
         "image": "./game-files/img/ducklife4.png"
     },
     {
-        "name": "btd",
+        "name": "Bloons Tower Defense",
         "type": "flash",
         "src": "./game-files/flash-files/btd.swf",
         "image": "./game-files/img/btd.png"
     },
     {
-        "name": "btd2",
+        "name": "Bloons Tower Defense 2",
         "type": "flash",
         "src": "./game-files/flash-files/btd2.swf",
         "image": "./game-files/img/btd2.png"
     },
     {
-        "name": "btd3",
+        "name": "Bloons Tower Defense 3",
         "type": "flash",
         "src": "./game-files/flash-files/btd3.swf", 
         "image": "./game-files/img/btd3.png"
