6325e3fd54f35b45739477a24dd27ca4bd710053
A1RG3MM <103892133+A1RG3MM@users.noreply.github.com>
2025-03-06 22:00:48 -0600

    geeked 2

--- public/js/main.js
+++ public/js/main.js
@@ -8,13 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
     loading.style.filter = "blur(0)";
 
     setTimeout(() => {
-        loading.style.transform = "scale(0.1)"
+        loading.style.transform = "scale(0.8)"
         loading.style.opacity = 0;
         setTimeout(() => {
             stage.style.transition = "0.5s ease";
             stage.style.opacity = 0;
             setTimeout(() => {
                 document.body.removeChild(stage);
+                document.querySelector('.clock').style.opacity = 1;
+                document.querySelector('.clock').style.transform = 'translate(-50%, -50%) scale(1)';
+                document.querySelector('.clock').style.filter = 'blur(0)';
                 document.querySelector('.bottom').style.bottom = 0;
                 document.querySelector('.bottom').style.opacity = 1;
                 document.querySelector('.bottom').style.transform = 'scale(1)';
